fbb88a8ed1df5d9f5925c7f0e599db154d173b42
AMcClean97 <andrewmcclean97@gmail.com>
2021-03-15 21:17:04 +0000

    "Added comments and temporary HTML file"

--- app.py
+++ app.py
@@ -6,24 +6,29 @@ import dbinfo
 
 app = Flask(__name__)
 
+#Basic test template
 @app.route("/")
 def index():
     return render_template("test.html", name = 'test')
 
+#for accessing map.html
 @app.route("/map")
 def map():
     return render_template("map.html")
 
+#for accessing list.html
 @app.route("/list")
 def list():
     return render_template("list.html")
 
+#Returns JSON data of stations table
 @app.route("/stations")
 def stations():
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
     df = pd.read_sql_table("stations", engine)
     return df.to_json(orient='records')#render_template("test.html", results = 'piss')
 
+#Returns JSON of most recent weather data
 @app.route("/weather")
 def weather():
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
@@ -32,6 +37,7 @@ def weather():
         engine)
     return df.to_json(orient='records')
 
+# Returns JSON data of most recent  availability data
 @app.route("/availability")
 def availability():
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
