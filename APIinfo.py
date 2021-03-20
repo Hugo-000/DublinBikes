05ba51c47c282140ce810284516b511271738930
AMcClean97 <andrewmcclean97@gmail.com>
2021-03-20 17:24:58 +0000

    Improvements to the scraper program to allow for electrical bikes

--- APIinfo.py
+++ APIinfo.py
@@ -1,5 +1,5 @@
 BIKE_APIKEY = "6fe84493b9eae56453f78c93315a9c5a4b699842"
-BIKE_URI = "https://api.jcdecaux.com/vls/v1/stations"
+BIKE_URI = "https://api.jcdecaux.com/vls/v3/stations"
 BIKE_CONTRACT ="Dublin"
 WEATHER_APIKEY = "2057a02078eceb25297cab97460bb29b"
 DUBLIN_LATITUDE = "53.3498"
--- get_jcdecaux.py
+++ get_jcdecaux.py
@@ -4,9 +4,10 @@ import sqlalchemy as sqla
 from sqlalchemy import Table, Column, Integer, Float, String, MetaData, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker
 import requests
-from datetime import datetime
+import datetime as dt
 import json
 import time
+import pytz
 import traceback
 
 engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
@@ -14,7 +15,7 @@ engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USE
 def main():
     while True:
         try:
-            now = datetime.now(tz=pytz.timezone('Europe/Dublin')).time()
+            now = dt.datetime.now(tz=pytz.timezone('Europe/Dublin')).time()
             if now >= dt.time(4,30) or now <= dt.time(0,30):#Don't scrape when closed
                 #get data
                 response = requests.get(APIinfo.BIKE_URI, params={"apiKey": APIinfo.BIKE_APIKEY, "contract": APIinfo.BIKE_CONTRACT})
@@ -30,18 +31,21 @@ def main():
 
 #Parses data received. fixes n/a timestamps
 def parseData(obj):
-    try:
-        date = datetime.fromtimestamp(int(obj['last_update']/1e3))
-    except: #If last_update timestamp invalid, replace with current time.
+    if 'lastUpdate' not in obj or obj['lastUpdate'] == None:
         return None
-    return{'number': obj['number'],
-        'status' : obj['status'],
-        'bike_stands': obj['bike_stands'],
-        'available_bike_stands': obj['available_bike_stands'],
-        'available_bikes': obj['available_bikes'],
-        'last_update': date
-          }
-
+    else:
+        return{'number': obj['number'],
+               'status' : obj['status'],
+               'bike_stands': obj['totalStands']['capacity'],
+               'available_bike_stands': obj['totalStands']['availabilities']['stands'],
+               'available_bikes': obj['totalStands']['availabilities']['bikes'],
+               'mechanical_bikes': obj['totalStands']['availabilities']['mechanicalBikes'],
+               'electrical_bikes': obj['totalStands']['availabilities']['electricalBikes'],
+               'electrical_internal_battery_bikes' : obj['totalStands']['availabilities']['electricalInternalBatteryBikes'],
+               'electrical_removable_battery_bikes': obj['totalStands']['availabilities']['electricalRemovableBatteryBikes'],
+               'last_update': dt.datetime.strptime(obj['lastUpdate'], '20%y-%m-%dT%H:%M:%SZ')
+               }
+    
 #store bike data in database
 def store_to_db(json_data):
     metadata = sqla.MetaData(bind=engine)
