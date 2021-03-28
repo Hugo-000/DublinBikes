b09bf327bcac2d798167e2fc5d914fde3dcf7d6b
AMcClean97 <andrewmcclean97@gmail.com>
2021-03-28 18:32:27 +0100

    Added more data to info windows. Moved All Javascript to one file

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 from sqlalchemy import create_engine
 import pandas as pd
 import dbinfo
+import datetime as dt
+import pytz
 
 app = Flask(__name__)
 
@@ -16,20 +18,15 @@ def index():
 def map():
     return render_template("map.html")
 
-#for accessing list.html
-@app.route("/list")
-def list():
-    return render_template("list.html")
-
 #Returns JSON data of stations table
-@app.route("/stations")
+@app.route("/get_stations")
 def stations():
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
     df = pd.read_sql("SELECT * FROM stations", engine)
     return df.to_json(orient='records')#render_template("test.html", results = 'piss')
 
 #Returns JSON of most recent weather data
-@app.route("/weather")
+@app.route("/get_weather")
 def weather():
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
     df = pd.read_sql(
@@ -38,12 +35,20 @@ def weather():
     return df.to_json(orient='records')
 
 # Returns JSON data of most recent  availability data
-@app.route("/availability")
+@app.route("/get_availability")
 def availability():
     engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
     df = pd.read_sql("SELECT * FROM availability WHERE (number, last_update) in (select number, max(last_update) from dbbikes.availability group by number);", 
         engine)
     return df.to_json(orient='records')
-    
+
+#Gives One Week worth of data on a specific station
+@app.route("/get_availability/<int:station_id>")
+def week_availability(station_id):
+    engine = create_engine("mysql+mysqlconnector://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.URI, dbinfo.PORT, dbinfo.DB), echo=True)
+    now = dt.datetime.now(tz=pytz.timezone('Europe/Dublin'))
+    df = pd.read_sql("SELECT * FROM availability WHERE number = {} and last_update >= date_add('{}', interval -7 DAY) order by last_update desc;".format(station_id, now), engine)
+    return df.to_json(orient='records')
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/scripts.js
@@ -0,0 +1,86 @@
+DUBLIN_LAT = 53.349804;
+DUBLIN_LNG = -6.260310;
+
+let map;
+
+get_current_weather("weather");
+
+//Makes Map
+function initMap() {
+    fetch("/get_availability").then(response =>{   
+        return response.json();   
+    }).then(availability_data => {  
+    
+        fetch("/get_stations").then(response => {
+            return response.json();
+        }).then(stations => {
+            
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: { lat: DUBLIN_LAT, lng: DUBLIN_LNG },
+                zoom: 14,
+            });
+            
+            
+            stations.forEach(station => {
+                const marker = new google.maps.Marker({
+                    position: {lat: station.latitude, lng: station.longitude},
+                    map: map,
+                });
+                
+                availability_data.forEach(data => {
+                    if (station.number == data.number) {
+                        marker.addListener("click", () => {
+                            displayInfo(map, marker, station, data)
+                            makeGraphs(station)
+                        });
+                    }
+                })
+                
+            });
+        }).catch(err => {
+            console.log("error:",err);
+        });
+    }).catch(err => {
+        console.log("error:",err);
+    })
+}
+
+//Creates and Displays Info Window
+function displayInfo(map, marker, station, data) {
+    const infowindow = new google.maps.InfoWindow({
+        content: '<h1>Station ' + station.number + '</h1><h2>' + station.address +'</h2>'
+            + '<ul><li>' + data.status + '</li>'
+            + '<li>' + station.banking +  '</li>'
+            + '<li>Bikes: ' +  data.available_bikes +'/' + data.bike_stands + '<ul>'
+            + '<li>Mechanical: ' + data.mechanical_bikes
+            + '<li>Electrical: ' + data.electrical_bikes + '<ul>'
+            + '<li>Internal_Battery: ' + data.electrical_internal_battery_bikes + '</li>'
+            + '<li>Removable_Battery: ' + data.electrical_removable_battery_bikes + '</li></ul></li></ul></li>'
+            + '<li>Free Stands: ' + data.available_bike_stands +'/' + data.bike_stands + '</li>'
+        });
+        infowindow.open(map, marker);
+}
+
+//Creates Graph when given the Station NOT STARTED
+function makeGraphs(station){
+    console.log('ha');
+}
+
+function get_current_weather(id) {
+    fetch("/get_weather").then(response => response.json())
+        .then(function(data) {
+        var content = "";
+        var date = new Date(data[0]['time']);
+        content = "<h2>" + data[0]['main'] + "</h2>" +
+            "<ul><li>" + data[0]['description'] + 
+            "</li><li>Temperature: " + data[0]['temp'] + 
+            "C</li><li>Humidity: " + data[0]['humidity'] +
+            "%</li><li>Visibility: " + data[0]['visibility'] + 
+            "m</li><li>Wind Speed: " + data[0]['wind_speed'] +
+            "m/s</li></ul><h4>Time Taken: " + date + "</h4>"
+        document.getElementById(id).innerHTML = content;
+    })
+    .catch(err => {
+        console.log("Unable to collect/format current weather data");
+    })
+}
